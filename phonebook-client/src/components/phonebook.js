fefbe8e5e9fc9f1bb770da96f5e2a8623421a284
safarudinyoga <safarudinyoga@gmail.com>
2019-10-30 01:01:22 +0700

    adding form add

--- phonebook-client/src/components/phonebook.js
+++ phonebook-client/src/components/phonebook.js
@@ -1,39 +1,28 @@
 import React from 'react';
-// import ChatForm from './chatForm';
-import PhonebookItem from './phonebookItem';
-import { connect } from 'react-redux'
-import { LoadData } from '../actions';
+import PhonebookForm from '../containers/phonebookForm'
+import PhonebookItem from '../containers/phonebookItem';
+import SearchPhonebook from './searchphonebook'
 
-class Phonebook extends React.Component {
-    constructor(props) {
-        super(props);
-
-    }
-
-    componentDidMount() {
-        this.props.loadData()
-    }
+export default class Phonebook extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Todo</h1>
+            <div className="container">
+                <div className="card my-4">
+                    <div className="card-header text-center">
+                        <h1>Phonebook App</h1>
+                    </div>
+                </div>
+                {/* <button type="button" className="btn btn-info my-2 mr-2"><i className="fas fa-plus"></i> Add</button>
+                <button type="button" className="btn btn-outline-info my-2 mr-2"> <i className="fas fa-search"></i> Search</button> */}
+                <PhonebookForm />
+                {/* <SearchPhonebook /> */}
                 <PhonebookItem data={this.props.data} />
-                {/* <ChatForm /> */}
+
+                <div className="card-footer text-center text-muted">
+                    <i className="far fa-copyright"></i> Safarudin Alwi Prayogo
+                </div>
             </div>
         )
     }
 }
-
-const mapStateToProps = (state) => ({
-    data: state.todos
-})
-
-const mapDispatchToProps = (dispatch) => ({
-    loadData: () => dispatch(LoadData())
-})
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Phonebook)
