fdd0648103031a3a9a0e1fa142caf9fbb8df1cda
safarudinyoga <safarudinyoga@gmail.com>
2019-10-30 00:59:59 +0700

    adding add and delete

--- phonebook-client/src/reducers/todos.js
+++ phonebook-client/src/reducers/todos.js
@@ -1,12 +1,48 @@
+import {
+    LOAD_DATA_SUCCESS,
+    LOAD_DATA_FAILURE,
+    ADD_DATA,
+    ADD_DATA_SUCCESS,
+    ADD_DATA_FAILURE,
+    DELETE_DATA,
+    DELETE_DATA_SUCCESS,
+    DELETE_DATA_FAILURE
+} from "../constants/actiontype"
+
 const todos = (state = [], action) => {
 
     switch (action.type) {
 
-        case 'LOAD_DATA_SUCCESS':
-            return action.data.map((data) => {
-                // data.sent = true;
+        case LOAD_DATA_SUCCESS:
+            return action.todos.map((data) => {
+                data.sent = true;
                 return data
             })
+
+        case ADD_DATA:
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    name: action.name,
+                    phonenumber: action.phonenumber,
+                    sent: true
+                }
+            ]
+        
+        case ADD_DATA_SUCCESS:
+            return action.todos.map((data) => {
+                data.sent = true;
+                return data
+            })
+
+        case DELETE_DATA:
+            return state.filter((data) => data.id !== action.id)
+
+        case DELETE_DATA_SUCCESS:
+        case DELETE_DATA_FAILURE:
+        case ADD_DATA_FAILURE:
+        case LOAD_DATA_FAILURE:
         default:
             return state
     }
