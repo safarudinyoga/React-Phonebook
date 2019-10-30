51f65578690d235dd9289f7968fbc7767a6f3a0f
safarudinyoga <safarudinyoga@gmail.com>
2019-10-31 03:08:09 +0700

    adding Search feature

--- phonebook-client/src/reducers/todos.js
+++ phonebook-client/src/reducers/todos.js
@@ -6,18 +6,32 @@ import {
     ADD_DATA_FAILURE,
     DELETE_DATA,
     DELETE_DATA_SUCCESS,
-    DELETE_DATA_FAILURE
+    DELETE_DATA_FAILURE,
+    SEARCH_DATA,
+    SEARCH_DATA_RESET
 } from "../constants/actiontype"
+
+// sort method in frontend
+const compare = (a,b) => {
+    if ( a.name < b.name ){
+        return -1;
+      }
+      if ( a.name > b.name ){
+        return 1;
+      }
+      return 0;
+}
 
 const todos = (state = [], action) => {
 
     switch (action.type) {
 
         case LOAD_DATA_SUCCESS:
-            return action.todos.map((data) => {
-                data.sent = true;
-                return data
-            })
+            return action.todos.map((data) => ({
+                ...data,
+                sent: true,
+                isVisible: true
+            })).sort(compare)
 
         case ADD_DATA:
             return [
@@ -26,19 +40,32 @@ const todos = (state = [], action) => {
                     id: action.id,
                     name: action.name,
                     phonenumber: action.phonenumber,
-                    sent: true
+                    sent: true,
+                    isVisible: true
                 }
-            ]
+            ].sort(compare)
         
         case ADD_DATA_SUCCESS:
-            return action.todos.map((data) => {
+            return state.map((data) => {
                 data.sent = true;
                 return data
-            })
+            }).sort(compare)
 
         case DELETE_DATA:
             return state.filter((data) => data.id !== action.id)
 
+        case SEARCH_DATA:
+            return state.map((item) => ({
+                ...item, 
+                isVisible: (item.name.toLowerCase().includes(action.value) || item.phonenumber.includes(action.value))
+            }))
+
+        case SEARCH_DATA_RESET:
+            return state.map((item) => ({
+                ...item,
+                isVisible: true
+            }))
+        
         case DELETE_DATA_SUCCESS:
         case DELETE_DATA_FAILURE:
         case ADD_DATA_FAILURE:
