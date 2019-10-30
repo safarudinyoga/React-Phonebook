c84b9b4641076a77323a7255aa20408e583504b7
safarudinyoga <safarudinyoga@gmail.com>
2019-10-31 03:05:16 +0700

    Search feature done

--- phonebook-client/src/components/phonebook.js
+++ phonebook-client/src/components/phonebook.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import PhonebookForm from '../containers/phonebookForm'
+import PhonebookForm from '../containers/phonebookForm';
 import PhonebookItem from '../containers/phonebookItem';
-import SearchPhonebook from './searchphonebook'
+import SearchPhonebook from '../containers/searchphonebook';
 
 export default class Phonebook extends React.Component {
 
@@ -13,12 +13,9 @@ export default class Phonebook extends React.Component {
                         <h1>Phonebook App</h1>
                     </div>
                 </div>
-                {/* <button type="button" className="btn btn-info my-2 mr-2"><i className="fas fa-plus"></i> Add</button>
-                <button type="button" className="btn btn-outline-info my-2 mr-2"> <i className="fas fa-search"></i> Search</button> */}
                 <PhonebookForm />
-                {/* <SearchPhonebook /> */}
+                <SearchPhonebook />
                 <PhonebookItem data={this.props.data} />
-
                 <div className="card-footer text-center text-muted">
                     <i className="far fa-copyright"></i> Safarudin Alwi Prayogo
                 </div>
