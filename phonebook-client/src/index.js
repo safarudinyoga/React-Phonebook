960e350db348f5c25519a6dfb27811c6f0941348
safarudinyoga <safarudinyoga@gmail.com>
2019-10-31 03:09:15 +0700

    adding middleware for redux

--- phonebook-client/src/index.js
+++ phonebook-client/src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import Phonebook from './components/phonebook';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './reducers';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(
