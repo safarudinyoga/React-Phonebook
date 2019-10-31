9b228566f41420983a3e247cc8686fb0d51c66c9
safarudinyoga <safarudinyoga@gmail.com>
2019-11-01 00:36:14 +0700

    100%

--- phonebook-client/src/components/item.js
+++ phonebook-client/src/components/item.js
@@ -3,14 +3,13 @@ import React from 'react';
 function Item(props) {
     return (
         <tr>
-            {console.log(props.sent)}
             <th scope="row">{props.index}</th>
             <th scope="row" className="text-left">{props.name}</th>
             <th scope="row">{props.phonenumber}</th>
             <td>
                 {props.sent ? (
                     <div>
-                        <button type="button" className="btn btn-success mr-2"><i
+                        <button type="button" className="btn btn-success mr-2" onClick={props.onEdit}><i
                             className="fas fa-pencil-alt"></i> Edit</button>
                         <button type="button" className="btn btn-danger" onClick={props.onDelete}><i className="fas fa-trash"></i> Delete</button>
                     </div>
