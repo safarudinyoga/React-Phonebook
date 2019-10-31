10457be344d7ced86ca408941c30bc41de770385
safarudinyoga <safarudinyoga@gmail.com>
2019-10-31 20:10:42 +0700

    Resend Done

--- phonebook-client/src/components/item.js
+++ phonebook-client/src/components/item.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
 function Item(props) {
-    return <tr>
-        <th scope="row">{props.index}</th>
-        <th scope="row" className="text-left">{props.name}</th>
-        <th scope="row">{props.phonenumber}</th>
-        <td>
-            <button type="button" className="btn btn-success mr-2"><i
-                className="fas fa-pencil-alt"></i> Edit</button>
-            <button type="button" className="btn btn-danger" onClick={props.onDelete}><i className="fas fa-trash"></i> Delete</button>
-        </td>
-    </tr>
+    return (
+        <tr>
+            {console.log(props.sent)}
+            <th scope="row">{props.index}</th>
+            <th scope="row" className="text-left">{props.name}</th>
+            <th scope="row">{props.phonenumber}</th>
+            <td>
+                {props.sent ? (
+                    <div>
+                        <button type="button" className="btn btn-success mr-2"><i
+                            className="fas fa-pencil-alt"></i> Edit</button>
+                        <button type="button" className="btn btn-danger" onClick={props.onDelete}><i className="fas fa-trash"></i> Delete</button>
+                    </div>
+                ) :
+                <button type="button" onClick={props.onResend} className="btn btn-warning"><i
+                className="fas fa-sync-alt"></i> Resend</button>
+                }
+            </td>
+        </tr>
+    )
 }
 
 export default Item;
