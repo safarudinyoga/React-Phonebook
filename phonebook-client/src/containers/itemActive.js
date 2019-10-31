c83fb939f7d9df7331c541a8060fb99ede4f7ca0
safarudinyoga <safarudinyoga@gmail.com>
2019-11-01 00:37:58 +0700

    100%

--- phonebook-client/src/containers/itemActive.js
+++ phonebook-client/src/containers/itemActive.js
@@ -3,6 +3,7 @@ import { deleteData, resendData } from '../actions';
 import Item from '../components/item';
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
+    onEdit: () => dispatch(editON(ownProps.id)),
     onDelete: () => dispatch(deleteData(ownProps.id)),
     onResend: () => dispatch(resendData(ownProps.id, ownProps.name, ownProps.phonenumber))
 })
